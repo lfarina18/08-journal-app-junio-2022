a6fbeefb10ed8d6438f72b9b40bc9cdd92f2c797
Leonardo Farina <lfarina@ticmas.com>
2022-07-27 09:01:42 -0300

    Editando LoginPage

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,10 +1,12 @@
-import { BrowserRouter } from "react-router-dom";
+
 import { AppRouter } from "./router/AppRouter"
+import { AppTheme } from "./theme"
 
 export const JournalApp = () => {
   return (
-    <BrowserRouter>
+    <AppTheme>
       <AppRouter />
-    </BrowserRouter>
+    </AppTheme>
+
   )
 }
